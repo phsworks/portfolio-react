a42b2d2324f3bac6ceba1c80765b9790d4164df7
Peedee01 <piethein@schouten.nl>
2024-12-27 14:36:15 +0100

    project image

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,18 @@
+import "./Footer.css"
+import { NavLink } from "react-router-dom";
+import logo from "../assets/Logo.png";
+
 function Footer(){
 
   return (
-    <h1>footer</h1>
-  )
+    <div className="footer-container">
+      <div className="logo">
+        <NavLink to="/">
+          <img width={55} src={logo} alt="logo" />
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 
 export default Footer
