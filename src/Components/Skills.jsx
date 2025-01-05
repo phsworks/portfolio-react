bc3f15e82d0cdb0aca20ce07ec7f4ca41d341ab0
Peedee01 <piethein@schouten.nl>
2025-01-05 09:25:50 +0100

    skilss fixed

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -2,14 +2,14 @@ import "./Skills.css";
 import Javascript from "../assets/Javascript.png";
 import HTML from "../assets/HTML.png";
 import CSS from "../assets/CSS.png";
-import React from "../assets/React.png";
+import ReactLogo from "../assets/React.png";
 import Express from "../assets/Express.js.png";
 import MongoDB from "../assets/MongoDB.png";
 import Wordpress from "../assets/Wordpress.png";
 import MySQL from "../assets/MySQL.png";
 import NodeJS from "../assets/NodeJS.png";
 import Git from "../assets/Git.png";
-import Github from "../assets/Github.png"
+import GithubLogo from "../assets/GithubLogo.png"
 
 function Skills() {
   return (
@@ -39,7 +39,7 @@ function Skills() {
           </div>
           <div className="skill-container">
             <div className="skill-card">
-              <img width={100} src={React} alt="React" />
+              <img width={100} src={ReactLogo} alt="React" />
             </div>
             <p>React</p>
           </div>
@@ -49,44 +49,39 @@ function Skills() {
             </div>
             <p>Git</p>
           </div>
-           <div className="skill-container">
+          <div className="skill-container">
             <div className="skill-card">
-              <img width={100} src={Github} alt="Github" />
+              <img width={100} src={GithubLogo} alt="Git" />
             </div>
             <p>Github</p>
-           </div>
+          </div>
           <div className="skill-container">
             <div className="skill-card">
               <img width={100} src={Express} alt="Express" />
-              <p>92%</p>
             </div>
             <p>Express</p>
           </div>
           <div className="skill-container">
             <div className="skill-card">
               <img width={100} src={Wordpress} alt="Wordpress" />
-              <p>92%</p>
             </div>
             <p>Wordpress</p>
           </div>
           <div className="skill-container">
             <div className="skill-card">
               <img width={100} src={MySQL} alt="MySQL" />
-              <p>92%</p>
             </div>
             <p>MySQL</p>
           </div>
           <div className="skill-container">
             <div className="skill-card">
               <img width={100} src={NodeJS} alt="NodeJS" />
-              <p>92%</p>
             </div>
             <p>NodeJS</p>
           </div>
           <div className="skill-container">
             <div className="skill-card">
               <img width={100} src={MongoDB} alt="NodeJS" />
-              <p>92%</p>
             </div>
             <p>MongoDB</p>
           </div>
