576c0acf4e730d5d89557b58eeb553d3a8eb5ecd
Peedee01 <piethein@schouten.nl>
2025-01-08 10:46:43 +0100

    social icons added

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import "./Hero.css";
 import Piet from "../assets/Piet-Hein.png";
 import { NavLink } from "react-router-dom";
+import github from "../assets/GithubLogo.png";
+import linkedin from "../assets/linkedin.png";
+import instagram from "../assets/instagram.png";
+import youtube from "../assets/youtube.png";
 
 function Hero() {
   const cvDownload = () => {
@@ -15,7 +19,8 @@ function Hero() {
         <div className="personal-info">
           <h2> I am Piet-Hein,</h2>
           <h1>
-            Passionate Full-Stack <span>Web Developer</span>{" "}
+            Passionate Full-Stack <br></br>
+            <span>Web Developer</span>{" "}
           </h1>
 
           <img
@@ -29,26 +34,42 @@ function Hero() {
             combine creativity, problem-solving, and a results-driven approach
             to bring ideas to life.”
           </p>
-          <div className="info-buttons">
-            <button onClick={cvDownload} className="secondary-button">
-              Download CV
-              <span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="13"
-                  viewBox="0 0 20 22"
-                >
-                  <path
-                    fill="#02609a"
-                    d="M12 2a1 1 0 0 1 1 1v10.586l2.293-2.293a1 1 0 0 1 1.414 1.414l-4 4a1 1 0 0 1-1.414 0l-4-4a1 1 0 1 1 1.414-1.414L11 13.586V3a1 1 0 0 1 1-1M5 17a1 1 0 0 1 1 1v2h12v-2a1 1 0 1 1 2 0v2a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-2a1 1 0 0 1 1-1"
-                  />
-                </svg>
-              </span>
-            </button>
-            <button className="primary-button">
-              <NavLink to="/aboutme"> About Me</NavLink>
-            </button>
+          <div className="connect-section">
+            <div className="info-buttons">
+              <button onClick={cvDownload} className="secondary-button">
+                Download CV
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="13"
+                    viewBox="0 0 20 22"
+                  >
+                    <path
+                      fill="#02609a"
+                      d="M12 2a1 1 0 0 1 1 1v10.586l2.293-2.293a1 1 0 0 1 1.414 1.414l-4 4a1 1 0 0 1-1.414 0l-4-4a1 1 0 1 1 1.414-1.414L11 13.586V3a1 1 0 0 1 1-1M5 17a1 1 0 0 1 1 1v2h12v-2a1 1 0 1 1 2 0v2a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-2a1 1 0 0 1 1-1"
+                    />
+                  </svg>
+                </span>
+              </button>
+              <button className="primary-button">
+                <NavLink to="/aboutme"> About Me</NavLink>
+              </button>
+            </div>
+            <div className="social-icons">
+              <a href="https://github.com/phsworks?tab=repositories">
+                <img width={30} src={github} alt="github-logo" />
+              </a>
+              <a href="https://www.youtube.com/channel/UCEIjn71x-ahkmjvUxYURz-w">
+                <img width={30} src={youtube} alt="youtube-logo" />
+              </a>
+              <a href="https://www.instagram.com/phsdevelopment/">
+                <img width={30} src={instagram} alt="instagram-logo" />
+              </a>
+              <a href="https://www.linkedin.com/in/piethein-schouten/">
+                <img width={30} src={linkedin} alt="linkedin-logo" />
+              </a>
+            </div>
           </div>
         </div>
         <div className="personal-picture">
