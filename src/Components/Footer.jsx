22eb8eaae1a5d7718da2c785fe82b7d904c36d0f
Peedee01 <piethein@schouten.nl>
2025-01-10 13:32:19 +0100

    footer responsive

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,52 +1,55 @@
-import "./Footer.css"
+import "./Footer.css";
 import { NavLink } from "react-router-dom";
 import { HashLink as Link } from "react-router-hash-link";
-import logo from "../assets/Logo-mobile.png";
+import mobilelogo from "../assets/PHS-mobile.svg";
+import desktoplogo from "../assets/PHS-desktop.svg"
 
-function Footer(){
-
+function Footer() {
   const scrollUp = () => {
     window.scrollTo({
       top: 0,
       left: 0,
       behavior: "smooth",
     });
-  }
+  };
 
   return (
     <div className="footer-container">
-      <div className="logo-footer">
-        <NavLink to="/" onClick={scrollUp}>
-          <img src={logo} alt="logo" />
-        </NavLink>
+      <div className="footer-items">
+        <div className="logo-footer">
+          <NavLink to="/" onClick={scrollUp}>
+            <img className="mobile-logo" src={mobilelogo} alt="mobile-logo" />
+            <img className="desktop-logo" src={desktoplogo} alt="desktop-logo" />
+          </NavLink>
+        </div>
+        <div className="footer-menu">
+          <nav>
+            <ul className="footer-links">
+              <li>
+                <NavLink to="/aboutme"> About Me</NavLink>
+              </li>
+              <li>
+                <Link onClick={scrollUp} to="/#skills-section">
+                  Skills
+                </Link>
+              </li>
+              <li>
+                <Link onClick={scrollUp} to="/#projects-section">
+                  Projects
+                </Link>
+              </li>
+              <li>
+                <Link onClick={scrollUp} to="/#contact-section">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
-      <div className="footer-menu">
-        <nav>
-          <ul className="footer-links">
-            <li>
-              <NavLink to="/aboutme"> About Me</NavLink>
-            </li>
-            <li>
-              <Link onClick={scrollUp} to="/#skills-section">
-                Skills
-              </Link>
-            </li>
-            <li>
-              <Link onClick={scrollUp} to="/#projects-section">
-                Projects
-              </Link>
-            </li>
-            <li>
-              <Link onClick={scrollUp} to="/#contact-section">
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <small> © 2025 ALl Rights Reserved by PHS Development</small>
-      </div>
+      <small> © 2025 ALl Rights Reserved by PHS Development</small>
     </div>
   );
 }
 
-export default Footer
+export default Footer;
