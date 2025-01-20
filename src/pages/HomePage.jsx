363a2ed7dcf90cdcae55dc2a66436546c3ce61a6
Peedee01 <piethein@schouten.nl>
2025-01-20 16:12:22 +0100

    profile-picture changed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 import scrollUpArrow from "../assets/arrowUp.svg"
 import "./HomePage.css";
 
+
 function HomePage() {
   const [isVisible, setIsVisible] = useState(false);
 
