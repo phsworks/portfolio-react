d94c490057fec02a1cbe60ff99737714dab3813f
Peedee01 <piethein@schouten.nl>
2025-02-13 21:44:22 +0100

    i18n import added app

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Navbar from "./Components/Navbar";
 import AboutPage from "./pages/AboutPage";
 import ErrorPage from "./pages/ErrorPage";
 import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n";
 
 function App() {
   return (
