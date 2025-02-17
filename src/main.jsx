60deebba206feb0c4d316d04e3f379c0b254b598
Peedee01 <piethein@schouten.nl>
2025-02-17 14:52:10 +0100

    browserRouter config

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./i18n";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router>
+    <BrowserRouter>
       <App />
-    </Router>
+    </BrowserRouter>
   </StrictMode>
 );
