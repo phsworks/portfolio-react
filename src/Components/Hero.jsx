0a900383a49b911e9fe0ad1701c8934db7c02028
phsworks <piethein@schouten.nl>
2025-09-17 15:18:06 +0200

    new cv link

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -13,7 +13,7 @@ function Hero() {
 
   const cvDownload = () => {
     window.location.href =
-      "https://drive.google.com/file/d/1ErEvxnL7sGkcbJgJNx7ZwNkBJ4lIVJwK/view?usp=drive_link";
+      "https://drive.google.com/file/d/1k7SfC5OCU3gxzWJdPkGxzwAixI87GMIz/view?usp=sharing";
   };
 
   const scrollUp = () => {
