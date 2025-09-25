ba23b833f91a0bffa3e284e1ae8a3acd9672b287
phsworks <piethein@schouten.nl>
2025-09-25 18:12:52 +0200

    timeline animation added

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -22,8 +22,23 @@ function AboutPage() {
 
   useEffect(() => {
     const toggleVisibility = () => {
+      const timelineElement1 = document.querySelector(".timeline-1");
+      const timelineElement2 = document.querySelector(".timeline-2");
+      const timelineElement3 = document.querySelector(".timeline-3");
+      const timelineElement4 = document.querySelector(".timeline-4");
+
       if (window.scrollY > 300) {
         setIsVisible(true);
+        if (window.scrollY > 400) {
+          timelineElement1.classList.add("timeline-animation");
+          timelineElement2.classList.add("timeline-animation");
+          timelineElement3.classList.add("timeline-animation-2");
+          timelineElement4.classList.add("timeline-animation-2");
+
+        }
+
+
+
       } else {
         setIsVisible(false);
       }
@@ -32,6 +47,8 @@ function AboutPage() {
     window.addEventListener("scroll", toggleVisibility);
     return () => window.removeEventListener("scroll", toggleVisibility);
   }, []);
+
+
 
   return (
     <div id="about-section">
@@ -66,39 +83,44 @@ function AboutPage() {
           <div class="timeline-middle">
             <div class="timeline-circle"></div>
           </div>
-          <div class="timeline-component timeline-content">
-            <h3> {t("entrepeneurship")}</h3>
-            <i>2021-2023</i>
-            <p>{t("propaedeuticDiploma")}</p>
-            <img
-              className="story-pictures"
-              width={120}
-              src={diploma}
-              alt="diploma"
-              fetchPriority="high"
-            />
+          <div class="timeline-1">
+            <div class="timeline-component timeline-content">
+              <h3> {t("entrepeneurship")}</h3>
+              <i>2021-2023</i>
+              <p>{t("propaedeuticDiploma")}</p>
+              <img
+                className="story-pictures"
+                width={120}
+                src={diploma}
+                alt="diploma"
+                fetchPriority="high"
+              />
+            </div>
           </div>
-          <div class="timeline-component timeline-content">
-            <h3> {t("wordpress")}</h3>
-            <i>2023-2024</i>
-            <p>{t("trendGigant")}</p>
-            <img
-              className="story-pictures"
-              width={120}
-              src={trendgigant}
-              alt="trendgigant"
-              fetchPriority="high"
-            />
-            <p>{t("solvware")}</p>
+          <div class="timeline-2">
+            <div class="timeline-component timeline-content">
+              <h3> {t("wordpress")}</h3>
+              <i>2023-2024</i>
+              <p>{t("trendGigant")}</p>
+              <img
+                className="story-pictures"
+                width={120}
+                src={trendgigant}
+                alt="trendgigant"
+                fetchPriority="high"
+              />
+              <p>{t("solvware")}</p>
 
-            <img
-              className="story-pictures"
-              width={120}
-              src={developer}
-              alt="developer"
-              fetchPriority="high"
-            />
+              <img
+                className="story-pictures"
+                width={120}
+                src={developer}
+                alt="developer"
+                fetchPriority="high"
+              />
+            </div>
           </div>
+
           <div class="timeline-middle">
             <div class="timeline-circle"></div>
           </div>
@@ -107,30 +129,33 @@ function AboutPage() {
           <div class="timeline-middle">
             <div class="timeline-circle"></div>
           </div>
-          <div class="timeline-component timeline-content">
-            <h3> Web Development Bootcamp</h3>
-            <i>2024</i>
-            <p>{t("ironhack")}</p>
-            <img
-              className="story-pictures"
-              width={120}
-              src={webdev}
-              alt="ironhack"
-              fetchPriority="high"
-            />
-            <img
-              className="story-pictures"
-              width={120}
-              src={present}
-              alt="presentation ironhack"
-              fetchPriority="high"
-            />
+          <div class="timeline-3">
+            <div class="timeline-component timeline-content">
+              <h3> Web Development Bootcamp</h3>
+              <i>2024</i>
+              <p>{t("ironhack")}</p>
+              <img
+                className="story-pictures"
+                width={120}
+                src={webdev}
+                alt="ironhack"
+                fetchPriority="high"
+              />
+              <img
+                className="story-pictures"
+                width={120}
+                src={present}
+                alt="presentation ironhack"
+                fetchPriority="high"
+              />
+            </div>
           </div>
-
-          <div class="timeline-component timeline-content">
-            <h3> {t("create")}</h3>
-            <i>2025</i>
-            <p>{t("creativeTech")}</p>
+          <div class="timeline-4">
+            <div class="timeline-component timeline-content">
+              <h3> {t("create")}</h3>
+              <i>2025</i>
+              <p>{t("creativeTech")}</p>
+            </div>
           </div>
         </div>
       </section>
